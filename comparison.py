aa987b6e1ec808a7f5595600c1d5ad11f843e299
Shef <121624316+shefinvex@users.noreply.github.com>
2022-12-31 13:07:16 +0330

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -5,7 +5,7 @@ platform_quant_df
 platform_quality_df = pd.read_csv('./input_data/platform_total_quality.csv', usecols=[0,1])
 platform_quality_df.columns = ['org_id', 'pl_quality_score']
 
-a = total_score_df.round(2).merge(items_prepared[['org_id','national_id']].drop_duplicates(subset=['org_id','national_id']), how='left', on='org_id')
+a = total_score_df.round(2).merge(items_prepared[['org_id','unique_id']].drop_duplicates(subset=['org_id','unique_id']), how='left', on='org_id')
 a = a.merge(platform_quant_df, how='left', on='org_id').merge(platform_quality_df, how='left', on='org_id')
 a['pl_total_score'] = a['pl_quant_score'] + a['pl_quality_score']
 a['quant_delta'] = a['pl_quant_score'] - a['quantitative']
